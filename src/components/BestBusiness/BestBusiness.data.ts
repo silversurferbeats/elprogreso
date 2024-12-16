8180b6fcbf1cf6ec4521b151e789fca1c65da295
silversurferbeats <gabrielsuarez89@gmail.com>
2024-12-16 18:45:26 -0300

    feat(BestBusiness.data): add navideña card and update price

--- src/components/BestBusiness/BestBusiness.data.ts
+++ src/components/BestBusiness/BestBusiness.data.ts
@@ -3,46 +3,54 @@ import { BestBusinessData } from "./BestBusiness.types";
 export const dataFeaturesBusiness: BestBusinessData = [
   {
     id: 1,
+    img: "/assets/image10.jpeg",
+    title: "Picada Navideña",
+    description:
+      "Sidra 1888, Pan de Campo, Jamón crudo, Queso Dambo, Salame, Cantimpalo, Lomo con hierbas, Longaniza, Mortadela, Pategras, Roquefort, Olivas rellenas, Olivas negras, Cebollín, Pepinillos. Comen 5, pican 7. Picada sola $38.000. Picada con Sidra del Valle $40.000",
+    price: "$ 43.000",
+  },
+  {
+    id: 2,
     img: "/assets/image8.png",
     title: "Picada XXXL",
     description:
       "Bandeja de 70cm. Pan de Campo + 12 pancitos, Salame, Lomo Ahumado, Jamón Cocido, Queso Roquefort, Longaniza, Cantimpalo, Mortadela calcha, Salame Criollo, Queso Paulina, Cantimpalo Tipo Español, Primavera, Bondiola, Pepinillos, Olivas Rellenas, Olivas Negras, Cebollin. Comen 15, Pican 18",
-    price: "$ 61.600",
+    price: "$ 61.000",
   },
   {
-    id: 2,
+    id: 3,
     img: "/assets/doblePremium.png",
     title: "Picada XXL",
     description:
       "2 pan de campo + 2 bandejas, Queso Paulina, Salame,  Lomo Ahumado, Jamón Cocido, Queso Roquefort, Longaniza, Cantimpalo, Mortadela Calcha, Primavera, Bondiola, Pepinillos, Olivas Rellenas, Olivas Negras, Cebollin. Comen 10, Pican 12",
-    price: "$ 57.000",
+    price: "$ 63.000",
   },
   {
-    id: 3,
+    id: 4,
     img: "/assets/premium.png",
     title: "Picada Premium",
     description:
       "Pan de Campo + Salame, Lomo Ahumado, Jamón Cocido, Queso Roquefort, Pategras, Longaniza, Cantimpalo, Mortadela Calcha, Queso Paulina, Primavera, Bondiola, Pepinillos, Olivas Rellenas, Olivas Negras, Cebollin. Comen 7, Pican 8 ",
-    price: "$ 35,200",
+    price: "$ 36.000",
   },
   {
-    id: 4,
+    id: 5,
     img: "/assets/especial.png",
     title: "Picada Especial",
     description:
       "Pan de Campo +  Jamón Cocido, Bondiola, Cantimpalo, Queso Paulina, Salame, Lomo Ahumado, Queso Roquefort, Longaniza, Mortadela Calchaquí, Pategras Saborizados, Drip de Queso Cremoso, Olivas Rellenas, Olivas Negras. Comen 5, Pican 7",
-    price: "$ 31,800",
+    price: "$ 33.000",
   },
   {
-    id: 5,
+    id: 6,
     img: "/assets/deCampo.png",
     title: "Picada Campo",
     description:
       "Pan de Campo + Bondiola, Queso Paulina, Salame Criollo, Lomo Ahumado, Jamón Cocido, Queso Roquefort, Longaniza, Cantimpalo Tipo Español, Pategras Saborizados, Pepinillos, Olivas Rellenas, Olivas Negras. Comen 4, Pican 5",
-    price: "$ 27.500",
+    price: "$ 29.000",
   },
   {
-    id: 6,
+    id: 7,
     img: "/assets/gourmet.png",
     title: "Picada Gourmet",
     description:
@@ -50,7 +58,7 @@ export const dataFeaturesBusiness: BestBusinessData = [
     price: "$ 25.000",
   },
   {
-    id: 7,
+    id: 8,
     img: "/assets/chica.png",
     title: "Picada Chica",
     description:
@@ -58,7 +66,7 @@ export const dataFeaturesBusiness: BestBusinessData = [
     price: "$ 19.500",
   },
   {
-    id: 8,
+    id: 9,
     img: "/assets/image9.png",
     title: "Combo Para Regalar",
     description:
@@ -66,14 +74,14 @@ export const dataFeaturesBusiness: BestBusinessData = [
     price: "$ --",
   },
   {
-    id: 9,
+    id: 10,
     img: "/assets/tablaQuebracho.png",
     title: "Tabla de Quebracho",
     description: "Toni grande 40 x 20 cm.",
     price: "$ 15.000",
   },
   {
-    id: 10,
+    id: 11,
     img: "/assets/tablaQuebracho.png",
     title: "Tabla de Quebracho",
     description: "Toni chica 30 x 16 cm.",
